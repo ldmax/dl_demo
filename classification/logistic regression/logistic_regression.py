0ce3a9d502ded7661a486fe5bb0acb3a7a399efe
lidan <lidanmax@163.com>
2018-02-24 08:39:21 +0800

    use model to classify test data and print error

--- classification/logistic regression/logistic_regression.py
+++ classification/logistic regression/logistic_regression.py
@@ -18,8 +18,6 @@ Created on Thu Feb 22 21:10:36 2018
 def data_prepare(path):
     # reading data
     dataframe = pd.read_csv(path)
-    label = dataframe["income"]
-    dataframe.drop(["income"], axis=1, inplace=True)
     
     # convert categorical data into numeric data
     dataframe["workclass"] = dataframe["workclass"].astype("category")
@@ -29,12 +27,18 @@ def data_prepare(path):
     dataframe["race"] = dataframe["race"].astype("category")
     dataframe["sex"] = dataframe["sex"].astype("category")
     dataframe["native_country"] = dataframe["native_country"].astype("category")
+    if len(dataframe.columns) == 15:
+        dataframe["income"] = dataframe["income"].astype("category")
+
     cat_columns = dataframe.select_dtypes(["category"]).columns
     dataframe[cat_columns] = dataframe[cat_columns].apply(lambda x: x.cat.codes)  # x: a Series in dataframe
-    label = label.astype("category")
-    label = label.cat.codes
     # drop column "education"
     dataframe.drop(["education"], axis=1, inplace=True)
+    
+    y = np.zeros((dataframe.shape[0], 1))
+    if len(dataframe.columns) == 14:
+        y = dataframe["income"].as_matrix()
+        dataframe.drop(["income"], axis=1, inplace=True)
     
     # scaling and normalization for multi-feature data set
     # scaling: divided by max value of the column
@@ -47,7 +51,6 @@ def data_prepare(path):
     
     # convert DataFrame to ndarray so that matrix manipulation can be conveyed
     x = dataframe.as_matrix()
-    y = label.as_matrix()
     return x, y
 ##################################
 #    logistic regression
@@ -78,7 +81,7 @@ def gradient_decent(lr, iteration, x, y):
 ##################################
 
 
-x, y = data_prepare("../train.csv")
+x, y = data_prepare("https://ntumlta.github.io/2017fall-ml-hw2/raw_data/train.csv")
 x = np.concatenate((np.ones((x.shape[0], 1)), x), axis=1)  # adding bias
 lr = 10  # learning rate initialization
 iteration = 10000
@@ -87,4 +90,27 @@ w = gradient_decent(lr, iteration, x, y)
 ##################################
 #       test
 ##################################
+
+
+def sigmoid(z):
+    return 1 / (1 + math.exp(-z))
+
+
+x_test, y_test = data_prepare("https://ntumlta.github.io/2017fall-ml-hw2/raw_data/test.csv")
+x_test = np.concatenate((np.ones((x_test.shape[0], 1)), x_test), axis=1)  # adding bias
+z_test = np.dot(x_test, w)
+income = []
+for i in z_test.flat:
+    income.append(sigmoid(i))
+
+income = np.array(income)
+# error calculation
+
+y_for = pd.read_csv("https://ntumlta.github.io/2017fall-ml-hw2/correct_answer.csv")
+y_for.label = income
+y_for["label"] = y_for["label"].apply(lambda x: 1 if x>0.9 else 0)
+
+y_real = pd.read_csv("https://ntumlta.github.io/2017fall-ml-hw2/correct_answer.csv")
+err = abs(y_for.label-y_real.label).sum()/len(y_real.label)
+print(err)
 
